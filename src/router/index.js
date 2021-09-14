d3b9eea3eea82837486002461f7c648118bc1d5b
Battal Şahin <73367836+vbattalshn@users.noreply.github.com>
2021-09-14 14:36:23 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from "../views/Home"
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import("../views/Home")
+    component: Home
   },
 ]
 
